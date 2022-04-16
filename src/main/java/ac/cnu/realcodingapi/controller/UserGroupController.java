cd6b58eeef99f2c8c0c1a54204671b3d5853b577
junwon6 <rh3junwon@gmail.com>
2022-04-16 17:02:01 +0900

    test

--- src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
@@ -3,7 +3,7 @@ package ac.cnu.realcodingapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//
 @RestController
 public class UserGroupController {
     @GetMapping("/api")
